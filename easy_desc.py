175c41b2f2ddb2f9eb4e36330bba5f3a8b68f316
ad3ph <a.golomolzin@g.nsu.ru>
2023-07-07 11:56:44 +0700

    fixed errors

--- easy_desc.py
+++ easy_desc.py
@@ -1,9 +1,11 @@
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 
+import pandas as pd
 from pandas import DataFrame
 from rdkit.ML.Descriptors import MoleculeDescriptors 
-
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
 
 def add_desc_to_df(df: DataFrame, desc: list):
@@ -29,7 +31,7 @@ def fill_nan_desc(df: DataFrame, non_desc_cols: list = None):
     cols_to_drop = ['SMILES']
     cols_to_drop.extend(non_desc_cols)
     for col in df.drop(columns=cols_to_drop).columns:
-        col_type = data[col].dtype
+        col_type = df[col].dtype
         calc = MoleculeDescriptors.MolecularDescriptorCalculator([col])
         mask = df[col].isna()
         df.loc[mask, col] = df.loc[mask, 'SMILES'].map(
